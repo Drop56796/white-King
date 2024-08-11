8a2350d4c5ece4b2d041684519f0951a9d4f5207
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-08-11 17:33:45 +0800

    Update White King.lua

--- White King.lua
+++ White King.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Splix"))()
+local library = loadstring(game:HttpGet("https://github.com/Drop56796/CreepyEyeHub/blob/main/Splix.lua?raw=true"))()
 
 -- Create a new window
 local window = library:new({
